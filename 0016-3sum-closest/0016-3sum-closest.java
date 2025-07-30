7d09f36f03ce0c51cfa9e7a198b5f54620da4e13
S.Jey Agash <jeyagash256@gmail.com>
2025-07-30 14:18:58 +0530

    Time: 14 ms (71.3%), Space: 44 MB (8.78%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,36 +1,24 @@
 import java.util.Arrays;
+
 public class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        if (nums == null || nums.length < 3) {
-            throw new IllegalArgumentException("Input array must have at least 3 elements.");
-        }
-        Arrays.sort(nums);
-        int closest = nums[0] + nums[1] + nums[2];
+        Arrays.sort(nums); 
+        int n = nums.length;
+        int closest = nums[0] + nums[1] + nums[2]; 
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1, right = n - 1;
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-             int left = i + 1;
-            int right = nums.length - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                if (sum == target) {
-                    return sum;
-                }
+                 if (sum == target) return sum;
 
-                if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                if (Math.abs(target - sum) < Math.abs(target - closest)) {
                     closest = sum;
                 }
-
                 if (sum < target) {
                     left++;
-                    while (left < right && nums[left] == nums[left - 1]) {
-                        left++;
-                    }
                 } else {
                     right--;
-                    while (left < right && nums[right] == nums[right + 1]) {
-                        right--;
-                    }
                 }
             }
         }
